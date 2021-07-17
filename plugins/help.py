eb5c3a13890ede76d05d69a8ce412fd88405a55b
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-18 00:58:49 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,7 +4,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	Lasiya2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("Youtube tutorial", url="https://youtu.be/l6rTppg3a1w")],
+		[InlineKeyboardButton("🤖 Other Bots 🤖", url="https://t.me/epusthakalaya_bots/7")],
 		 ])
-	help_image = "https://telegra.ph/file/27fefe183e306ef919437.jpg"
-	await message.reply_photo(help_image,  caption="**♔English help**\n• Just Send your Youtube video url⛓ \n• And i will give Method list to select your choice😋\n\n**♔සිංහල උදව්**\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n\n • This bot is fully free.\n• Don't pay anyone for Bots like this. \n\nAlso use our file renamer @Renamerv12bot",reply_markup=Lasiya2)
+	help_image = "https://telegra.ph/file/bf093f4f785d57665fe16.jpg"
+	await message.reply_photo(help_image,  caption="**🆘 Help Menu 🆘**\n• Just Send your Youtube video url \n• And I will give Method list to select your choice. \n\ \n\nJoin @epusthakalaya_bots",reply_markup=Lasiya2)
